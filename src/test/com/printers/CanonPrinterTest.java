3fbd331c73f3772e50aca19e3a3e66eddbbbf772
sonyasemeryaga <semeriaha.sofiia@lll.kpi.ua>
2024-12-28 00:55:07 +0200

    Updated HPPrinterTest.java (unifiy names of the printers created in tests) and add CanonPrinterTest.java

--- src/test/com/printers/CanonPrinterTest.java
+++ src/test/com/printers/CanonPrinterTest.java
@@ -4,64 +4,65 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class HPPrinterTest {
+public class CanonPrinterTest {
     @Test
     public void givenPrinterWithSufficientResources_whenPrintCalled_thenResourcesDecrease() {
-        HPPrinter printer = new HPPrinter(10, 10);
-        printer.printHP();
-        String expected = "Ink Level: 9,0; Paper Count: 9";
+        CanonPrinter printer = new CanonPrinter(15, 20);
+        printer.printCanon();
+        String expected = "Ink Level: 14,0; Paper Count: 19";
         assertEquals(expected, printer.toString());
     }
 
     @Test
     public void givenPrinterWithoutResources_whenPrintCalled_thenResourcesRemainUnchanged() {
-        HPPrinter printer = new HPPrinter(0, 0);
+        CanonPrinter printer = new CanonPrinter(0, 0);
         assertFalse(printer.isReady());
-        printer.printHP();
+        printer.printCanon();
         assertEquals("Ink Level: 0,0; Paper Count: 0", printer.toString());
     }
 
     @Test
     public void givenPrinterWithSufficientResources_whenSaveInkModeCalled_thenResourcesDecrease() {
-        HPPrinter printer = new HPPrinter(10, 10);
-        printer.saveInkModeHP();
-        String expected = "Ink Level: 9,5; Paper Count: 9";
+        CanonPrinter printer = new CanonPrinter(12, 15);
+        printer.saveInkModeCanon();
+        String expected = "Ink Level: 11,5; Paper Count: 14";
         assertEquals(expected, printer.toString());
     }
 
     @Test
     public void givenPrinterWithoutResources_whenSaveInkModeCalled_thenResourcesRemainUnchanged() {
-        HPPrinter printer = new HPPrinter(0, 0);
+        CanonPrinter printer = new CanonPrinter(0, 0);
         assertFalse(printer.isReady());
-        printer.saveInkModeHP();
+        printer.saveInkModeCanon();
         assertEquals("Ink Level: 0,0; Paper Count: 0", printer.toString());
     }
 
     @Test
     public void givenPrinterWithoutResources_whenMaintenancePerformed_thenResourcesIncrease() {
-        HPPrinter printer = new HPPrinter(0, 0);
-        printer.performMaintenanceHP();
+        CanonPrinter printer = new CanonPrinter(0, 0);
+        printer.performMaintenanceCanon();
         String expected = "Ink Level: 5,0; Paper Count: 5";
         assertEquals(expected, printer.toString());
     }
 
     @Test
     public void givenPrinterWithSufficientResources_whenMaintenancePerformed_thenResourcesIncrease() {
-        HPPrinter printer = new HPPrinter(10, 10);
+        CanonPrinter printer = new CanonPrinter(8, 10);
         assertTrue(printer.isReady());
-        printer.performMaintenanceHP();
-        assertEquals("Ink Level: 15,0; Paper Count: 15", printer.toString());
+        printer.performMaintenanceCanon();
+        assertEquals("Ink Level: 13,0; Paper Count: 15", printer.toString());
     }
 
     @Test
     public void givenPrinterWithSufficientResources_whenChecked_thenIsReadyReturnsTrue() {
-        HPPrinter printer = new HPPrinter(10, 10);
+        CanonPrinter printer = new CanonPrinter(5, 7);
         assertTrue(printer.isReady());
     }
 
     @Test
     public void givenPrinterWithoutResources_whenChecked_thenIsReadyReturnsFalse() {
-        HPPrinter printer = new HPPrinter(0, 0);
+        CanonPrinter printer = new CanonPrinter(0, 0);
         assertFalse(printer.isReady());
     }
 }
+
