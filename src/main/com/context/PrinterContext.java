c067b07683a06ce1242364c16525033d3390f18c
sonyasemeryaga <semeriaha.sofiia@lll.kpi.ua>
2024-12-28 21:14:00 +0200

    Created check and helper methods in PrinterContext.java that will be used for each printer

--- src/main/com/context/PrinterContext.java
+++ src/main/com/context/PrinterContext.java
@@ -8,4 +8,32 @@ public class PrinterContext {
         this.inkLevel = inkLevel;
         this.paperCount = paperCount;
     }
+
+    public boolean isReady() {
+        if (inkLevel <= 0 || paperCount <= 0) {
+            System.out.println("Printer is not ready. Please check the resources.");
+            return false;
+        }
+        return true;
+    }
+
+    public void useResources(double ink, int paper) {
+        if (paper > paperCount || ink > inkLevel) {
+            System.out.println("Cannot use more resources than available.");
+            System.out.println("Please try again with different parameters or replenish the resources.");
+        } else {
+            paperCount -= paper;
+            inkLevel -= ink;
+        }
+    }
+
+    public void replenishResources(double ink, int paper) {
+        paperCount += paper;
+        inkLevel += ink;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Ink Level: %.1f, Paper Count: %d", inkLevel, paperCount);
+    }
 }
