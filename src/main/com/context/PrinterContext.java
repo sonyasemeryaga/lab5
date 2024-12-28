af6982942e56227132ee23ac230699956b90271f
sonyasemeryaga <semeriaha.sofiia@lll.kpi.ua>
2024-12-28 02:57:24 +0200

    Create PrinterContext.java, which would be used in the printers classes as a field (composition of the classes)

---
+++ src/main/com/context/PrinterContext.java
@@ -0,0 +1,11 @@
+package com.context;
+
+public class PrinterContext {
+    private double inkLevel;
+    private int paperCount;
+
+    public PrinterContext(double inkLevel, int paperCount) {
+        this.inkLevel = inkLevel;
+        this.paperCount = paperCount;
+    }
+}
